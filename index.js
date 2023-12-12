5299b7c288bfdbd5e962086da190b2a8d5b007ac
emnasamchi <emnasamchi26@gmail.com>
2023-12-12 10:55:43 +0100

    cp added

---
+++ index.js
@@ -0,0 +1,32 @@
+// var tab = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+
+// for (let i = 0; i < tab.length; i++) {
+//   console.log(tab[i]);
+//   for (let j = 0; j < tab[i].length; j++) {
+//     console.log(tab[i][j]);
+//   }
+// }
+
+
+
+var num = 5 
+function updateValue(value) {
+    num = value
+    console.log("Inside function: ", num);
+  }
+  
+  console.log("Before function: ", num);
+  
+  updateValue(10);
+  
+  console.log("After function: ", num);
+
+
+
+
+
+  
